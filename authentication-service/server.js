8f464ce178ed9c9a4ba849b9a7782873a968b0dc
Tyson Bradford <bradford1141@gmail.com>
2022-01-28 13:12:09 -0700

    new commit

--- authentication-service/server.js
+++ authentication-service/server.js
@@ -2,14 +2,23 @@ const express = require('express');
 
 const port = 3000;
 
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello browser');
 });
 
 app.post('/login', (req, res) =>{
-
+    console.log(JSON.stringify(req.body));
+    if(req.body.userName == "bradford1141" && req.body.password == "Run72ner!"){
+        res.send("Welcome!")
+    } else{
+        res.send("Who are you?");
+    }
 });
 
 app.listen(port, ()=>{});
