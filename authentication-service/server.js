3c18304fb82e3145191c9be15219a0e48c88d1a9
Tyson Bradford <bradford1141@gmail.com>
2022-01-28 13:40:15 -0700

    new commit

--- authentication-service/server.js
+++ authentication-service/server.js
@@ -12,9 +12,12 @@ app.get('/', (req, res) => {
     res.send('Hello browser');
 });
 
+var md5 = require('md5');
+console.log(md5('message'));
+
 app.post('/login', (req, res) =>{
     console.log(JSON.stringify(req.body));
-    if(req.body.userName == "bradford1141" && req.body.password == "ymca"){
+    if(req.body.userName == "bradford1141" && req.body.password == "46de89b0e37534254f58ef29a6719939"){
         res.send("Welcome!")
     } else{
         res.send("Who are you?");
